ad6facd0af1b2a3cae70af6ca1120db472c99f7f
Richard Musiol <mail@richard-musiol.de>
2014-03-18 11:51:47 +0100

    added support for reading original names

--- sourcemap.go
+++ sourcemap.go
@@ -18,11 +18,12 @@ type Map struct {
 }
 
 type Mapping struct {
+	GeneratedLine   int
+	GeneratedColumn int
 	OriginalFile    string
 	OriginalLine    int
 	OriginalColumn  int
-	GeneratedLine   int
-	GeneratedColumn int
+	OriginalName    string
 }
 
 func Read(r io.Reader) (*Map, error) {
@@ -50,32 +51,12 @@ func init() {
 func (m *Map) DecodedMappings() []*Mapping {
 	if m.decodedMappings == nil {
 		r := strings.NewReader(m.Mappings)
-		readVLQ := func() int {
-			v := 0
-			s := uint(0)
-			for {
-				b, _ := r.ReadByte()
-				o := base64decode[b]
-				if o == 0xff {
-					r.UnreadByte()
-					return 0
-				}
-				v += (o &^ 32) << s
-				if o&32 == 0 {
-					break
-				}
-				s += 5
-			}
-			if v&1 != 0 {
-				return -(v >> 1)
-			}
-			return v >> 1
-		}
+		var generatedLine = 1
+		var generatedColumn = 0
 		var originalFile = 0
 		var originalLine = 1
 		var originalColumn = 0
-		var generatedLine = 1
-		var generatedColumn = 0
+		var originalName = 0
 		for r.Len() != 0 {
 			b, _ := r.ReadByte()
 			if b == ',' {
@@ -87,12 +68,44 @@ func (m *Map) DecodedMappings() []*Mapping {
 				continue
 			}
 			r.UnreadByte()
+
+			count := 0
+			readVLQ := func() int {
+				v := 0
+				s := uint(0)
+				for {
+					b, _ := r.ReadByte()
+					o := base64decode[b]
+					if o == 0xff {
+						r.UnreadByte()
+						return 0
+					}
+					v += (o &^ 32) << s
+					if o&32 == 0 {
+						break
+					}
+					s += 5
+				}
+				count++
+				if v&1 != 0 {
+					return -(v >> 1)
+				}
+				return v >> 1
+			}
 			generatedColumn += readVLQ()
 			originalFile += readVLQ()
 			originalLine += readVLQ()
 			originalColumn += readVLQ()
-			readVLQ()
-			m.decodedMappings = append(m.decodedMappings, &Mapping{path.Join(m.SourceRoot, m.Sources[originalFile]), originalLine, originalColumn, generatedLine, generatedColumn})
+			originalName += readVLQ()
+
+			switch count {
+			case 1:
+				m.decodedMappings = append(m.decodedMappings, &Mapping{generatedLine, generatedColumn, "", 0, 0, ""})
+			case 4:
+				m.decodedMappings = append(m.decodedMappings, &Mapping{generatedLine, generatedColumn, path.Join(m.SourceRoot, m.Sources[originalFile]), originalLine, originalColumn, ""})
+			case 5:
+				m.decodedMappings = append(m.decodedMappings, &Mapping{generatedLine, generatedColumn, path.Join(m.SourceRoot, m.Sources[originalFile]), originalLine, originalColumn, m.Names[originalName]})
+			}
 		}
 	}
 	return m.decodedMappings
--- sourcemap_test.go
+++ sourcemap_test.go
@@ -26,21 +26,21 @@ func TestRead(t *testing.T) {
 		t.Error(m)
 	}
 	assertMapping := func(got, expected *Mapping) {
-		if got.OriginalFile != expected.OriginalFile || got.OriginalLine != expected.OriginalLine || got.OriginalColumn != expected.OriginalColumn || got.GeneratedLine != expected.GeneratedLine || got.GeneratedColumn != expected.GeneratedColumn {
+		if got.GeneratedLine != expected.GeneratedLine || got.GeneratedColumn != expected.GeneratedColumn || got.OriginalFile != expected.OriginalFile || got.OriginalLine != expected.OriginalLine || got.OriginalColumn != expected.OriginalColumn || got.OriginalName != expected.OriginalName {
 			t.Errorf("expected %v, got %v", expected, got)
 		}
 	}
-	assertMapping(mappings[0], &Mapping{"/the/root/one.js", 1, 1, 1, 1})
-	assertMapping(mappings[1], &Mapping{"/the/root/one.js", 1, 5, 1, 5})
-	assertMapping(mappings[2], &Mapping{"/the/root/one.js", 1, 11, 1, 9})
-	assertMapping(mappings[3], &Mapping{"/the/root/one.js", 1, 21, 1, 18})
-	assertMapping(mappings[4], &Mapping{"/the/root/one.js", 2, 3, 1, 21})
-	assertMapping(mappings[5], &Mapping{"/the/root/one.js", 2, 10, 1, 28})
-	assertMapping(mappings[6], &Mapping{"/the/root/one.js", 2, 14, 1, 32})
-	assertMapping(mappings[7], &Mapping{"/the/root/two.js", 1, 1, 2, 1})
-	assertMapping(mappings[8], &Mapping{"/the/root/two.js", 1, 5, 2, 5})
-	assertMapping(mappings[9], &Mapping{"/the/root/two.js", 1, 11, 2, 9})
-	assertMapping(mappings[10], &Mapping{"/the/root/two.js", 1, 21, 2, 18})
-	assertMapping(mappings[11], &Mapping{"/the/root/two.js", 2, 3, 2, 21})
-	assertMapping(mappings[12], &Mapping{"/the/root/two.js", 2, 10, 2, 28})
+	assertMapping(mappings[0], &Mapping{1, 1, "/the/root/one.js", 1, 1, ""})
+	assertMapping(mappings[1], &Mapping{1, 5, "/the/root/one.js", 1, 5, ""})
+	assertMapping(mappings[2], &Mapping{1, 9, "/the/root/one.js", 1, 11, ""})
+	assertMapping(mappings[3], &Mapping{1, 18, "/the/root/one.js", 1, 21, "bar"})
+	assertMapping(mappings[4], &Mapping{1, 21, "/the/root/one.js", 2, 3, ""})
+	assertMapping(mappings[5], &Mapping{1, 28, "/the/root/one.js", 2, 10, "baz"})
+	assertMapping(mappings[6], &Mapping{1, 32, "/the/root/one.js", 2, 14, "bar"})
+	assertMapping(mappings[7], &Mapping{2, 1, "/the/root/two.js", 1, 1, ""})
+	assertMapping(mappings[8], &Mapping{2, 5, "/the/root/two.js", 1, 5, ""})
+	assertMapping(mappings[9], &Mapping{2, 9, "/the/root/two.js", 1, 11, ""})
+	assertMapping(mappings[10], &Mapping{2, 18, "/the/root/two.js", 1, 21, "n"})
+	assertMapping(mappings[11], &Mapping{2, 21, "/the/root/two.js", 2, 3, ""})
+	assertMapping(mappings[12], &Mapping{2, 28, "/the/root/two.js", 2, 10, "n"})
 }
